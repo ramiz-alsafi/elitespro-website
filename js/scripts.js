c60d90c301b8084cdaaeca155687f120ea191554
ramiz-alsafi <108527889+ramiz-alsafi@users.noreply.github.com>
2025-06-22 02:55:19 +0300

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -30,3 +30,13 @@ document.addEventListener("DOMContentLoaded", () => {
     nav.classList.toggle("open");
   });
 });
+document.addEventListener("DOMContentLoaded", function () {
+  const toggle = document.querySelector(".mobile-nav-toggle");
+  const nav = document.querySelector("nav");
+
+  if (toggle && nav) {
+    toggle.addEventListener("click", () => {
+      nav.classList.toggle("active");
+    });
+  }
+});
